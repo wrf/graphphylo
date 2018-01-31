43f789c1b4be26f32aea826486c645ab485dc7f3
wfrancis <wfrancis@biology.sdu.dk>
2018-01-31 15:43:56 +0100

    plot phylobayes

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # graphphylo
 visualization of phylogenetic output files
+
+## plot_phylobayes_traces
+R script to plot several parameters from the [phylobayes](https://github.com/bayesiancook/pbmpi) trace files. This can be run on a pair of trace files, one from each chain. The output file is a `pdf` automatically named based on the name of the first file. So a hypothetical pair of files `matrix_chain_1.trace` and `matrix_chain_2.trace` will produce a file called `matrix_chain1.trace.pdf`.
+
+`Rscript plot_phylobayes_traces.R matrix_chain_1.trace matrix_chain_2.trace`
+
+By default, this will plot up to the number of iterations (plus 0.1 * the length of the dataset) where there is a value of log(L) that is 99% of the maximum value. There is typically very little change after this point, but the plot can be forced to an arbitrary number of iterations using a third term in the command line:
+
+`Rscript plot_phylobayes_traces.R matrix_chain_1.trace matrix_chain_2.trace 10000`
+
+![Day_CAT_GTR1.trace.png](https://github.com/wrf/graphphylo/blob/master/Day_CAT_GTR1.trace.png)
+
+The [Dayhoff recoded dataset](https://bitbucket.org/bzxdp/feuda_et_al_2017) from [Whelan et al 2017](https://www.nature.com/articles/s41559-017-0331-3): the chains were run for 25000 iterations, though there is essentially no change after 250, meaning it is potentially run 100x longer than it needed to be.
---
+++ plot_phylobayes_traces.R
@@ -0,0 +1,112 @@
+# plot phylobayes trace log likelihoods
+
+# usage:
+
+# Rscript plot_phylobayes_traces.R hif-pb1.trace hif-pb2.trace
+
+# add optional max iterations next:
+
+# Rscript plot_phylobayes_traces.R hif-pb1.trace hif-pb2.trace 10000
+
+args = commandArgs(trailingOnly=TRUE)
+trace1file = args[1]
+trace2file = args[2]
+othermax = args[3]
+optionname = args[4]
+
+#trace1file = "~/data/project/supplements/feuda2017/whelanD20/CAT_GTR1.trace"
+#trace2file = "~/data/project/supplements/feuda2017/whelanD20/CAT_GTR2.trace"
+trace1dat = read.table(trace1file, header=FALSE, sep="\t")
+trace2dat = read.table(trace2file, header=FALSE, sep="\t")
+
+#trace1file = "~/data/project/supplements/feuda2017/whelan2017/Day_CAT_GTR1.trace"
+#trace2file = "~/data/project/supplements/feuda2017/whelan2017/Day_CAT_GTR2.trace"
+
+#trace1file = "~/est/feuda2017/bzxdp-feuda_et_al_2017-e970b278280d/CHANG/CHANG_ANALYSES_SUMMARY_FILES/LG1.trace"
+#trace2file = "~/est/feuda2017/bzxdp-feuda_et_al_2017-e970b278280d/CHANG/CHANG_ANALYSES_SUMMARY_FILES/LG2.trace"
+
+### CHECK FOR ALTERNATE HEADER LINE
+if ( trace1dat[1,1]=="iter" ) {
+trace1dat = read.table(trace1file, header=TRUE, sep="\t")
+trace2dat = read.table(trace2file, header=TRUE, sep="\t")
+}
+
+
+paste("trace 1 of length", length(trace1dat[,1]) )
+paste("trace 2 of length", length(trace2dat[,1]) )
+
+logln1 = trace1dat[,4]
+minmaxlogln1 = range(logln1)
+minmaxlogln1
+firstsat1 = minmaxlogln1[1] - (minmaxlogln1[1] - minmaxlogln1[2]) * 0.99
+paste("first value at 99% of max is", firstsat1 )
+
+tracemax = max( c(length(trace1dat[,1]),length(trace2dat[,1])) )
+maxlength = min( min( which(logln1 >= firstsat1))+min(5000,round(length(trace1dat[,1])/10)), tracemax )
+#maxlength = 500
+### MAKE SURE MAX CANNOT GO MORE THAN TRACE LENGTH
+if (!is.na(othermax)) {
+	paste("given number of iterations is", othermax )
+	maxlength = min( c(as.numeric(othermax), tracemax ) )
+}
+paste("using", maxlength, "as max iterations in plot")
+
+llrange  = range( pretty( range(trace1dat[,4])) )
+serange  = range( pretty( range( c( trace1dat[,8],trace2dat[,8]) ) ) )
+lenrange = range( pretty( range( c( trace1dat[,5],trace2dat[,5]) ) ) )
+
+### LG AND WAG MODELS DO NOT HAVE 10 COLUMNS
+if (dim(trace1dat)[2] < 10) {
+rrrange  = range( pretty( range( c( trace1dat[,6],trace2dat[,6]) ) ) )
+} else {
+rrrange  = range( pretty( range( c( trace1dat[,10],trace2dat[,10]) ) ) )
+}
+
+### MAKE GRAPH
+outputfile = paste(trace1file,".pdf",sep="")
+#pdf(file="~/data/project/supplements/feuda2017/whelan2017/Day_CAT_GTR1.trace.pdf", width=8, height=7)
+pdf(file=outputfile, width=11, height=11)
+par(mfrow=c(2,2), mar=c(4.5,4.5,4,2) )
+plot( trace1dat[,1], trace1dat[,4], type='l', xlim=c(0, maxlength), ylim=llrange, lwd=3, col="#f1a34099", xlab="Iterations", ylab="log(L)" , main="", cex.lab=1.4 )
+lines( trace2dat[,1], trace2dat[,4]+10, lwd=3, col="#0571b099"  )
+text(maxlength*0.75, llrange[1] + diff(llrange) * 0.6, paste("Final iterations =",tracemax) , cex=1.5)
+legend(maxlength*0.65, llrange[1] + diff(llrange) * 0.4, legend=c("Trace 1", "Trace 2"), col=c("#f1a340", "#0571b0"), lwd=2, cex=1.4)
+
+plot( trace1dat[,1], trace1dat[,8], type='l', xlim=c(0, maxlength), ylim=serange, lwd=3, col="#f1a34099", xlab="Iterations", ylab="statent" , main="", cex.lab=1.4 )
+lines( trace2dat[,1], trace2dat[,8], lwd=3, col="#0571b099"  )
+text(maxlength*0.75, serange[1] + diff(serange) * 0.6, paste("Final iterations =",tracemax) , cex=1.5)
+legend(maxlength*0.65, serange[1] + diff(serange) * 0.9, legend=c("Trace 1", "Trace 2"), col=c("#f1a340", "#0571b0"), lwd=2, cex=1.4)
+
+### CHECK FOR OPTIONAL LABEL NAME
+if (!is.na(optionname)) {
+mtext( paste(optionname, trace1file,"+",trace2file), side=3, cex=1.4, at=0-maxlength*0.2, line=1)
+} else {
+mtext( paste(trace1file,"+",trace2file), side=3, cex=1.4, at=0-maxlength*0.2, line=1)
+}
+
+plot( trace1dat[,1], trace1dat[,5], type='l', xlim=c(0, maxlength), ylim=lenrange, lwd=3, col="#f1a34099", xlab="Iterations", ylab="length" , main="", cex.lab=1.4 )
+lines( trace2dat[,1], trace2dat[,5], lwd=3, col="#0571b099"  )
+text(maxlength*0.75, lenrange[1] + diff(lenrange) * 0.6, paste("Final iterations =",max( c(length(trace1dat[,1]),length(trace2dat[,1])) )) , cex=1.5)
+legend(maxlength*0.65, lenrange[1] + diff(lenrange) * 0.4, legend=c("Trace 1", "Trace 2"), col=c("#f1a340", "#0571b0"), lwd=2, cex=1.4)
+
+### MAKE DIFFERENT PLOTS FOR RRENT AND ALPHA
+if (dim(trace1dat)[2] < 10) {
+plot( trace1dat[,1], trace1dat[,6], type='l', xlim=c(0, maxlength), ylim=rrrange, lwd=3, col="#f1a34099", xlab="Iterations", ylab="alpha" , main="", cex.lab=1.4 )
+lines( trace2dat[,1], trace2dat[,6], lwd=3, col="#0571b099"  )
+text(maxlength*0.75, rrrange[1] + diff(rrrange) * 0.6, paste("Final iterations =",max( c(length(trace1dat[,1]),length(trace2dat[,1])) )) , cex=1.5)
+legend(maxlength*0.65, rrrange[1] + diff(rrrange) * 0.9, legend=c("Trace 1", "Trace 2"), col=c("#f1a340", "#0571b0"), lwd=2, cex=1.4)
+} else {
+plot( trace1dat[,1], trace1dat[,10], type='l', xlim=c(0, maxlength), ylim=rrrange, lwd=3, col="#f1a34099", xlab="Iterations", ylab="rrent" , main="", cex.lab=1.4 )
+lines( trace2dat[,1], trace2dat[,10], lwd=3, col="#0571b099"  )
+text(maxlength*0.75, rrrange[1] + diff(rrrange) * 0.6, paste("Final iterations =",max( c(length(trace1dat[,1]),length(trace2dat[,1])) )) , cex=1.5)
+legend(maxlength*0.65, rrrange[1] + diff(rrrange) * 0.9, legend=c("Trace 1", "Trace 2"), col=c("#f1a340", "#0571b0"), lwd=2, cex=1.4)
+
+}
+
+
+dev.off()
+
+
+
+
+#
